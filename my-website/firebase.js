991a8690128a862e0e26dbeb5ddc28574ccb1630
juliet-karpah <122952544+juliet-karpah@users.noreply.github.com>
2023-08-08 17:01:55 -0700

    add firebase

---
+++ my-website/firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "./config";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
